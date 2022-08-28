2e9217c37329866afb2981d6b6a401590c5fb07a
Florian GRONDIN <grondin.florian@protonmail.com>
2022-08-28 15:29:37 +0200

    Display raw date and time in App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup>
-import GreetingsHeader from "./components/GreetingsHeader.vue";
-import TheEvents from "./components/TheEvents.vue";
-</script>
-
 <template>
   <header>
     <img
@@ -15,26 +10,91 @@ import TheEvents from "./components/TheEvents.vue";
 
     <div class="wrapper">
       <GreetingsHeader msg="Elyon Timer" />
+      <div id="iso-date-time">
+        <div id="iso-date">
+          {{ isoDate }}
+        </div>
+        <div id="iso-time">
+          {{ isoTime }}
+        </div>
+      </div>
     </div>
   </header>
 
   <main>
-    <TheEvents />
+    <TheEvents :iso-timestamp="isoTimestamp" />
   </main>
 </template>
 
-<style scoped>
+<script>
+const CURRENT_ZONE = "Europe/Paris";
+const CURRENT_LOCALE = "en";
+
+import GreetingsHeader from "./components/GreetingsHeader.vue";
+import TheEvents from "./components/TheEvents.vue";
+import { DateTime } from "luxon";
+
+export default {
+  components: {
+    GreetingsHeader,
+    TheEvents,
+  },
+
+  data() {
+    return {
+      isoDateTime: DateTime.now()
+        .setZone(CURRENT_ZONE)
+        .setLocale(CURRENT_LOCALE),
+    };
+  },
+
+  computed: {
+    isoDate() {
+      return this.isoDateTime.toLocaleString(DateTime.DATE_MED);
+    },
+
+    isoTime() {
+      return this.isoDateTime.toLocaleString(DateTime.TIME_24_WITH_SECONDS);
+    },
+
+    isoTimestamp() {
+      return this.isoDateTime.toMillis();
+    },
+  },
+
+  mounted() {
+    this.timerInterval = setInterval(() => {
+      this.updateIsoDateTime();
+    }, 1000);
+  },
+
+  beforeUnmount() {
+    clearInterval(this.timerInterval);
+  },
+
+  methods: {
+    updateIsoDateTime() {
+      this.isoDateTime = DateTime.now()
+        .setZone(CURRENT_ZONE)
+        .setLocale(CURRENT_LOCALE);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
 header {
   line-height: 1.5;
   display: flex;
   place-items: center;
   padding-right: calc(var(--section-gap) / 2);
   justify-content: center;
-}
-header .wrapper {
-  display: flex;
-  place-items: flex-start;
-  flex-wrap: wrap;
+
+  .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 
 .logo {
