16b46f2907ae0a95daf58eaf95e184344e15e431
Florian GRONDIN <grondin.florian@protonmail.com>
2022-09-03 09:53:50 +0200

    Defnine all events of each weekday

--- src/mixins/common.js
+++ src/mixins/common.js
@@ -18,13 +18,17 @@ export const genEvent = ({
   startMinutes,
   endMinutes,
   weekday = 0,
-  deltaDuration,
+  deltaDuration = false,
   duration,
   label,
 }) => {
   let startPlus = {};
   if (weekday) {
     startPlus = { days: weekday - isoDateTime.weekday };
+  }
+
+  if (!deltaDuration) {
+    deltaDuration = { hours: startHours, minutes: startMinutes };
   }
 
   return {
@@ -54,4 +58,454 @@ export const events = {
       label: "Daily reset",
     }),
   },
+};
+
+export const weekEvents = (weekday) => {
+  return [
+    {
+      ...genEvent({
+        startHours: 0,
+        endHours: 1,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 1,
+        endHours: 2,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 3,
+        endHours: 4,
+        startMinutes: 30,
+        endMinutes: 30,
+        weekday,
+        duration: { hours: 1 },
+        label: "Pirate ships",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 7,
+        endHours: 8,
+        startMinutes: 30,
+        endMinutes: 30,
+        weekday,
+        duration: { hours: 1 },
+        label: "Pirate ships",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 9,
+        endHours: 10,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 10,
+        endHours: 11,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 11,
+        endHours: 13,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 12,
+        endHours: 14,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Sacredwood",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 13,
+        endHours: 14,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 13,
+        endHours: 14,
+        startMinutes: 30,
+        endMinutes: 30,
+        weekday,
+        duration: { hours: 1 },
+        label: "Pirate ships",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 14,
+        endHours: 16,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 16,
+        endHours: 17,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 17,
+        endHours: 18,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 17,
+        endHours: 18,
+        startMinutes: 30,
+        endMinutes: 30,
+        weekday,
+        duration: { hours: 1 },
+        label: "Pirate ships",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 18,
+        endHours: 20,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Sacredwood",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 19,
+        endHours: 20,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 20,
+        endHours: 20,
+        startMinutes: 10,
+        endMinutes: 50,
+        weekday,
+        duration: { minutes: 40 },
+        label: "RvR",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 21,
+        endHours: 22,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 22,
+        endHours: 23,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 23,
+        endHours: 0,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Frozen city",
+      }),
+    },
+  ];
+};
+
+export const weekendEvents = (weekday) => {
+  return [
+    {
+      ...genEvent({
+        startHours: 0,
+        endHours: 1,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 1,
+        endHours: 2,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 3,
+        endHours: 4,
+        startMinutes: 30,
+        endMinutes: 30,
+        weekday,
+        duration: { hours: 1 },
+        label: "Pirate ships",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 7,
+        endHours: 8,
+        startMinutes: 30,
+        endMinutes: 30,
+        weekday,
+        duration: { hours: 1 },
+        label: "Pirate ships",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 9,
+        endHours: 10,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 10,
+        endHours: 11,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 11,
+        endHours: 13,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 12,
+        endHours: 14,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Sacredwood",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 13,
+        endHours: 14,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 13,
+        endHours: 14,
+        startMinutes: 30,
+        endMinutes: 30,
+        weekday,
+        duration: { hours: 1 },
+        label: "Pirate ships",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 14,
+        endHours: 16,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 16,
+        endHours: 17,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 17,
+        endHours: 18,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 17,
+        endHours: 18,
+        startMinutes: 30,
+        endMinutes: 30,
+        weekday,
+        duration: { hours: 1 },
+        label: "Pirate ships",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 18,
+        endHours: 20,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 2 },
+        label: "Sacredwood",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 19,
+        endHours: 20,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 20,
+        endHours: 21,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "GvG",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 21,
+        endHours: 22,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Frozen city",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 22,
+        endHours: 23,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Weekly dimension",
+      }),
+    },
+    {
+      ...genEvent({
+        startHours: 23,
+        endHours: 0,
+        startMinutes: 0,
+        endMinutes: 0,
+        weekday,
+        duration: { hours: 1 },
+        label: "Frozen city",
+      }),
+    },
+  ];
 };
--- src/mixins/wednesday.js
+++ src/mixins/wednesday.js
@@ -3,54 +3,5 @@ import * as common from "./common";
 const WEEKDAY = 3;
 
 export default () => {
-  return [
-    {
-      ...common.genEvent({
-        startHours: 3,
-        endHours: 4,
-        startMinutes: 30,
-        endMinutes: 0,
-        weekday: WEEKDAY,
-        deltaDuration: { hours: 3, minutes: 0 },
-        duration: { minutes: 30 },
-        label: "Pirate ships",
-      }),
-    },
-    {
-      ...common.genEvent({
-        startHours: 13,
-        endHours: 14,
-        startMinutes: 30,
-        endMinutes: 0,
-        weekday: WEEKDAY,
-        deltaDuration: { hours: 13, minutes: 0 },
-        duration: { minutes: 30 },
-        label: "Pirate ships",
-      }),
-    },
-    {
-      ...common.genEvent({
-        startHours: 17,
-        endHours: 18,
-        startMinutes: 30,
-        endMinutes: 0,
-        weekday: WEEKDAY,
-        deltaDuration: { hours: 17, minutes: 0 },
-        duration: { minutes: 30 },
-        label: "Pirate ships",
-      }),
-    },
-    {
-      ...common.genEvent({
-        startHours: 20,
-        endHours: 20,
-        startMinutes: 10,
-        endMinutes: 50,
-        weekday: WEEKDAY,
-        deltaDuration: { hours: 20, minutes: 10 },
-        duration: { minutes: 40 },
-        label: "RvR",
-      }),
-    },
-  ];
+  return common.weekEvents(WEEKDAY);
 };
